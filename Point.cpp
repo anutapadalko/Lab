517789511ee420bc97d2345ad0d282455ba100e1
Anna Padalko <anutapadalko@gmail.com>
2025-09-23 01:17:28 +0300

    Add point realisation

--- Point.cpp
+++ Point.cpp
@@ -1,1 +1,48 @@
 #include "Point.h"
+
+Point& Point::operator=(const Point& other)
+{
+    if (this != &other)
+    {
+        x_ = other.x_;
+        y_ = other.y_;
+    }
+    return *this;
+}
+
+double Point::getX() const
+{
+    return x_;
+}
+
+double Point::getY() const
+{
+    return y_;
+}
+
+void Point::setX(double x)
+{
+    x_ = x;
+}
+
+void Point::setY(double y)
+{
+    y_ = y;
+}
+
+void Point::setPoint(double x, double y)
+{
+    x_ = x;
+    y_ = y;
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& p)
+{
+    os << std::fixed << std::setprecision(6) << p.x_ << " " << p.y_;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Point& p) {
+    is >> p.x_ >> p.y_;
+    return is;
+}
--- Point.h
+++ Point.h
@@ -1,5 +1,31 @@
 #pragma once
+
+#include <iostream>
+#include <iomanip>
+
 class Point
 {
+private:
+    double x_;
+    double y_;
+
+public:
+    Point() : x_(0.0), y_(0.0) {}
+    Point(double x, double y) : x_(x), y_(y) {}
+
+    Point(const Point& other) : x_(other.x_), y_(other.y_) {}
+
+    Point& operator=(const Point& other);
+
+    double getX() const;
+    double getY() const;
+
+    void setX(double x);
+    void setY(double y);
+    void setPoint(double x, double y);
+
+    friend std::ostream& operator<<(std::ostream& os, const Point& p);
+
+    friend std::istream& operator>>(std::istream& is, Point& p);
 };
 
